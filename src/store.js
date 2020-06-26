86a24830c8406b56e6e56a80e2de33ebb31543f7
AJBates2 <aj.bates2.92@gmail.com>
2020-06-26 01:43:30 -0500

    working

---
+++ src/store.js
@@ -0,0 +1,37 @@
+module.exports = [
+    {
+        id: "cjozyzcil0000lxygs3gyg2mr",
+        title: "Thinkful",
+        url: "https://www.thinkful.com",
+        description: "Think outside the classroom",
+        rating: 5
+    },
+    {
+        id: "cjozyzeqh0001lxygb8mhnvhz",
+        title: "Google",
+        url: "https://www.google.com",
+        description: "Where we find everything else",
+        rating: 4
+    },
+    {
+        id: "cjkzyzeqh0001lxygb8mhqvh3",
+        title: "MDN",
+        url: "https://developer.mozilla.org",
+        description: "The only place to find web documentation",
+        rating: 5
+    },
+    {
+        id: "ckbvj0fhy000004preekok5vl",
+        title: "chabad.org",
+        url: "https://www.chabad.org",
+        description: "",
+        rating: 1
+    },
+    {
+        id: "ckbvjq90m000104pr49ngru9y",
+        title: "test",
+        url: "https://www.google.com",
+        description: "test",
+        rating: 1
+    }
+]
